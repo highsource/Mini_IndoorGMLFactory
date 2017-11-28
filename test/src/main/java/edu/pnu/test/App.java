f4983419af8fba783ab8313845941128aebe2eb1
JungHam <wjdgpal7@gmail.com>
2017-11-28 21:57:19 +0900

    first commit

---
+++ test/src/main/java/edu/pnu/test/App.java
@@ -0,0 +1,13 @@
+package edu.pnu.test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args ) throws Exception
+    {
+    	Binder.unmashaller.importIndoorGML("test","./Lotte_final");
+    }
+}
